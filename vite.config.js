7a4014616aba665c059933ecff4730a2d1fbe14d
MickWu <Mick_Wu@syscom.com.tw>
2025-04-24 11:26:12 +0800

    manifest不用套件的狀況

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({// ⚠️ 這邊請填你的 GitHub repo 名稱
-  base: '/my-app-pos/', 
+    base: '/my-app-pos/',
   plugins: [react()],
 })
