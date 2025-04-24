ba5e8181add80d10799e563e805bdd4b2ab0ccff
MickWu <Mick_Wu@syscom.com.tw>
2025-04-24 10:27:36 +0800

    deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig({// ⚠️ 這邊請填你的 GitHub repo 名稱
+  base: '/my-app-pos/', 
   plugins: [react()],
 })
